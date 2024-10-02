efcf77fc89a7896462f466b38d24eb252935f519
David Robert <contact@david-robert.fr>
2024-10-02 15:02:14 +0200

    Dependency inversion : OrderRepository

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -6,12 +6,28 @@ import { OrderItem } from './domain/entity/order-item.entity';
 import { CreateOrderService } from 'src/order/domain/use-case/create-order.service';
 import { OrderRepositoryInterface } from 'src/order/domain/port/order.repository.interface';
 import OrderRepositoryTypeOrm from 'src/order/infrastructure/order.repository';
+import { PayOrderService } from 'src/order/domain/use-case/pay-order.service';
+import { CancelOrderService } from 'src/order/domain/use-case/cancel-order.service';
+import { SetInvoiceAddressOrderService } from 'src/order/domain/use-case/set-invoice-address-order.service';
+import { SetShippingAddressOrderService } from 'src/order/domain/use-case/set-shipping-address-order.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Order, OrderItem])],
   controllers: [OrderController],
 
   providers: [
+    // j'enregistre mon repository en tant que service
+    OrderRepositoryTypeOrm,
+
+    // j'enregistre le service directement (pas besoin de faire de useFactory)
+    // pour celui là car il injecte directement le OrderRepositoryTypeOrm)
+    PayOrderService,
+    CancelOrderService,
+    SetInvoiceAddressOrderService,
+    SetShippingAddressOrderService,
+    // pour pouvoir gérer l'inversion de dépendance
+    // du service CreateOrderService
+    // j'utilise le système de useFactory de nest
     {
       // quand j'enregistre la classe CreateOrderService
       provide: CreateOrderService,
