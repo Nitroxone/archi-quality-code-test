53e0d7f0b8c0ea8136c7148928f41a1caee14320
David Robert <contact@david-robert.fr>
2024-10-03 14:13:51 +0200

    unit test: create order

--- src/order/application/use-case/create-order.service.spec.ts
+++ src/order/application/use-case/create-order.service.spec.ts
@@ -1,5 +1,6 @@
 import { CreateOrderService } from '../use-case/create-order.service';
 import { OrderRepositoryInterface } from '../../domain/port/persistance/order.repository.interface';
+import { BadRequestException } from '@nestjs/common';
 
 class OrderRepositoryFake {
   async save(order) {
@@ -14,20 +15,20 @@ describe("an order can't be created if the order have more than 5 item", () => {
   it('should return an error', async () => {
     const createOrderService = new CreateOrderService(orderRepositoryFake);
 
-    const createOrderOutput = await createOrderService.execute({
-      customerName: 'John Doe',
-      items: [
-        { productName: 'item 1', price: 10, quantity: 1 },
-        { productName: 'item 1', price: 10, quantity: 1 },
-        { productName: 'item 1', price: 10, quantity: 1 },
-        { productName: 'item 1', price: 10, quantity: 1 },
-        { productName: 'item 1', price: 10, quantity: 1 },
-        { productName: 'item 1', price: 10, quantity: 1 },
-      ],
-      shippingAddress: 'Shipping Address',
-      invoiceAddress: 'Invoice Address',
-    });
-
-    expect(createOrderOutput).rejects.toThrow();
+    await expect(
+      createOrderService.execute({
+        customerName: 'John Doe',
+        items: [
+          { productName: 'item 1', price: 10, quantity: 1 },
+          { productName: 'item 1', price: 10, quantity: 1 },
+          { productName: 'item 1', price: 10, quantity: 1 },
+          { productName: 'item 1', price: 10, quantity: 1 },
+          { productName: 'item 1', price: 10, quantity: 1 },
+          { productName: 'item 1', price: 10, quantity: 1 },
+        ],
+        shippingAddress: 'Shipping Address',
+        invoiceAddress: 'Invoice Address',
+      }),
+    ).rejects.toThrow();
   });
 });
